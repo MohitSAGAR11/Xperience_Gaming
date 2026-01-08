ee4a3cddcc5033a7643f657f975014927e0bacd4
Mohit Sagar <mohitsagar378@gmail.com>
2026-01-08 10:27:15 +0530

    Version Update

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.xperiencegaming.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
     }
 
